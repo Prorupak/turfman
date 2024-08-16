89eb8ff5a06d15adb2ba706a6dff355098cbd20e
Prorupak <rupaksapkota2000@gmail.com>
2024-08-16 12:37:36 +0545

    feat: Integrate MainAppModule into application

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.modules';
+import { MainAppModule } from './modules/main-app.modules';
 
 @Module({
-  imports: [CoreModule],
+  imports: [CoreModule, MainAppModule],
   controllers: [AppController],
   providers: [AppService],
 })
