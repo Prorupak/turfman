63b4ed0554216ad835290a4f516097a835d86ef1
Prorupak <rupaksapkota2000@gmail.com>
2024-08-16 18:37:19 +0545

    Refactor: Update server documentation

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -53,10 +53,8 @@ async function bootstrap() {
   // Swagger (OpenAPI Docs)
   // This can be accessed by visiting {SERVER_URL}/api/docs
   const config = new DocumentBuilder()
-    .setTitle('Reactive Resume')
-    .setDescription(
-      "Reactive Resume is a free and open source resume builder that's built to make the mundane tasks of creating, updating and sharing your resume as easy as 1, 2, 3.",
-    )
+    .setTitle('Buzz App')
+    .setDescription('')
     .addCookieAuth('Authentication', {
       type: 'http',
       in: 'cookie',
